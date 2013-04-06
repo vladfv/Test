37e15bbf56733ead45fe763fc51bc2a2aed87b28
brian story <bstory32@gmail.com>
2013-04-06 16:35:52 -0500

    adds app template

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-//Node function called each time a new HTTPrequest arrives
-function onRequest(req,res) {
-	res.writeHead(200,{'Content-Type':'text/plain'});
-	res.end('Hello World ');
-}
-//Listen for connectionson the port provided by the host process
-var server=http.createServer(onRequest).listen(process.env.PORT || 8080);
