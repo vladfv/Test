56fea8eaa7c863827d9600f8310df3604e29759f
brian story <bstory32@gmail.com>
2013-04-06 17:16:44 -0500

    renames index.jade to html

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express');
 var app = express();
 
 app.configure(function() {
-	app.set('view engine', 'jade');
+	app.set('view engine', 'express');
 	app.set('views', __dirname + '/views');
 	app.use(express.static(__dirname + '/public'));
 });
